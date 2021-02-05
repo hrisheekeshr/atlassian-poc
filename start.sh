524cd251cdb79e2d23ad53055675836f2954f7fb
EC2 Default User <ec2-user@ip-172-31-31-101.us-west-1.compute.internal>
2021-02-05 11:34:57 +0000

    modified start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,13 @@
-echo "Starting Elasticsearch Server"
-docker run -d -p 9200:9200 -e "discovery.type=single-node" elasticsearch:7.9.2
-echo "Waiting 30 seconds"
+echo "Stopping Elasticsearch Server"
+docker rm -f haystack-elastic
+
+echo "Chilling for 5 Seconds"
 sleep 5s
+
+echo "Starting Elasticsearch Server"
+docker run -d -p 9200:9200 --name haystack-elastic -e "discovery.type=single-node" elasticsearch:7.9.2
+echo "Waiting 30 seconds"
+sleep 30s
 
 echo "Installing Requirements"
 pip install -r requirements.txt
@@ -15,4 +21,4 @@ echo "Done"
 echo "Starting API Server"
 cd ..
 cd rest_api
-gunicorn rest_api.application:app -b 0.0.0.0:80 -k uvicorn.workers.UvicornWorker -t 300
+gunicorn rest_api.application:app -b 0.0.0.0:8000 -k uvicorn.workers.UvicornWorker -t 300
